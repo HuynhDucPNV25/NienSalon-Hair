158fd35226eb04ab207803cb3c3194652433c6eb
phatdeptraiii <vitphat350@gmail.com>
2023-10-19 19:57:27 +0700

    feat(TN-31):Create login by email, 70%

---
+++ src/Js/TN-31_Login-Email.js
@@ -0,0 +1,34 @@
+function loginWithEmail() {
+    var email = document.getElementById("loginEmail").value;
+    var password = document.getElementById("password").value;
+
+    if (email === "" || password === "") {
+        alert("Xin hãy nhập email và mật khẩu!");
+        return;
+    }
+
+    authenticateUser(email, password)
+        .then(function (response) {
+            if (response.ok) {
+                alert("Đăng nhập thành công");
+                document.getElementById("loginAccountName").value = "";
+                document.getElementById("loginPassword").value = "";
+                window.location.href = ""; 
+            } else {
+                alert("Email hoặc mật khẩu không chính xác");
+            }
+        })
+        .catch(function (error) {
+            alert("Đăng nhập thất bại: " + error);
+        });
+}
+
+function authenticateUser(email, password) {
+    return new Promise(function (resolve, reject) {
+        if (email === "example@example.com" && password === "password") {
+            resolve({ ok: true });
+        } else {
+            resolve({ ok: false });
+        }
+    });
+}
