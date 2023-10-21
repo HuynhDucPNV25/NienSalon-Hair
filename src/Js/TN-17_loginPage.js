0323dd9702fdc985a4b6d3c3ed9691d444b0e54d
phatdeptraiii <vitphat350@gmail.com>
2023-10-21 11:35:41 +0700

    fix(TN-4):Update login

--- src/Js/TN-17_loginPage.js
+++ src/Js/TN-17_loginPage.js
@@ -1,30 +1,56 @@
-const accountData = "https://pnv-hair.onrender.com/Account";
+const accountDataUrl = "https://pnv-hair.onrender.com/Account";
 
-async function login() {
-    try {
-        const response = await axios.get(accountData);
-        const data = response.data;
+const login = async () => {
+  try {
+    const response = await axios.get(accountDataUrl);
+    const accountData = response.data;
+    const accountName = document.getElementById("loginAccountName").value;
+    const password = document.getElementById("loginPassword").value;
 
-        let accountName = document.getElementById("loginAccountName").value;
-        let password = document.getElementById("loginPassword").value;
+    if (accountName === "" || password === "") {
+      alert("Xin hãy nhập tên đăng nhập và mật khẩu!");
+    } else {
+      const account = accountData.find(
+        item => item.accountName === accountName
+      );
+      if (account) {
+        const isPasswordMatch = password === account.password;
+        if (isPasswordMatch) {
+          const userData = {
+            id: account.id,
+            role: account.role,
+            accountName: accountName,
+            password: account.password,
+          };
+          localStorage.setItem("userData", JSON.stringify(userData));
+          alert("Đăng nhập thành công");
+          document.getElementById("loginAccountName").value = "";
+          document.getElementById("loginPassword").value = "";
 
-        if (accountName === "" || password === "") {
-            alert("Xin hãy nhập tên đăng nhập và mật khẩu!");
-        } else if (accountName == data.accountName || password == data.password) {
-            let userData = {
-                accountName: accountName,
-                password: password
-            };
-            localStorage.setItem("userData", JSON.stringify(userData));
+          checkLogin(accountData);
 
-            alert("Đăng nhập thành công");
-            document.getElementById("loginAccountName").value = "";
-            document.getElementById("loginPassword").value = "";
-            window.location.href = ""; // Chuyển sang trang người dùng hoặc admin
         } else {
-            alert("Sai tên đăng nhập hoặc mật khẩu!");
+          alert("Sai tên đăng nhập hoặc mật khẩu!");
         }
-    } catch (error) {
-        console.log('Đã xảy ra lỗi:', error);
+      } else {
+        alert("Sai tên đăng nhập hoặc mật khẩu!");
+      }
     }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//chưa check login được
+function checkLogin(accountData) {
+  const checkAcc = "admin";
+  const curent = JSON.parse(localStorage.getItem("userData"));
+  const data = accountData.find(item => item.role === curent.role);
+  if (data) {
+    if (checkAcc == "admin") {
+      window.location.href = "../src/html/TN-14_Admin.html"; //Chuyển đến admin
+    } else {
+      window.location.href = "../src/html/user.html"; //Chuyển đến user
+    }
+  }
 }
