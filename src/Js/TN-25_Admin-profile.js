dcccfa9b3d78a333a46dfc7d4aed3f67ca2d511c
HoThiSang <143240933+HoThiSang@users.noreply.github.com>
2023-11-04 15:28:42 +0700

    feat(TN-25): update admin (#23)

--- src/Js/TN-25_Admin-profile.js
+++ src/Js/TN-25_Admin-profile.js
@@ -1,45 +1,16 @@
 const adminData = 'https://pnv-hair.onrender.com/Account';
 
-// const fetchData = async () => {
-//   try {
-//     const response = await axios.get(adminData);
-//     const data = response.data;
-//     let tableRows = '';
-//     data.forEach(function (item) {
-//       if (item.role) {
-//         tableRows = `
-//           <h3>Thông tin</h3>
-//           <h6>Tên Admin: ${item.name} </h6>
-//           <h6>Tên tài khoản: ${item.accountName} </h6>
-//           <h6>Mật khẩu: ${item.password}</h6>
-//           <h6>Email: ${item.email}</h6>
-//           <h6>Số điện thoại: ${item.phone}</h6>
-//           <h6>Giới tính: ${item.sex}</h6>
-//         `;
-//       }
-//     });
-    
-//     const tableBody = document.getElementById('informationAdmin');
-//     tableBody.innerHTML = tableRows;
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// };
-
-// fetchData();
-
 async function loadData() {
   try {
     const response = await axios.get(adminData);
     const data = response.data;
-    console.log(data);
     data.forEach(element => {
-      if(!data.role){
+      if(element.role==true){
         document.querySelector('#fname').value = element.name;
         document.querySelector('#email').value = element.email;
         document.querySelector('#password').value = element.password;
         document.querySelector('#Phone').value = element.phone;
-        document.querySelector('#message').value = element.message;
+        document.querySelector('#sex').value = element.sex;
       }
     });
     
@@ -50,4 +21,3 @@ async function loadData() {
 }
 
 loadData();
-
